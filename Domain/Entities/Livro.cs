be3208ed102083a889e7cbdc549dc058d326443f
Renan Ribeiro <ribeirorm04@gmail.com>
2025-12-15 16:51:35 -0300

    Criacao table Livros

---
+++ Domain/Entities/Livro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Livro
+    {
+        [Key]
+        public Guid IdLivro { get; set; }
+
+        public string Nome { get; set; } = null!;
+        public string Autor { get; set; } = null!;
+        public string Edicao { get; set; } = null!;
+        public string Editora { get; set; } = null!;
+        public string ISBN { get; set; } = null!;
+        public string? Descricao { get; set; }
+        public DateTime DataPublicacao { get; set; }
+    }
+}
